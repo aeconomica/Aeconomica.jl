54d9f61823d25841897df1f55fd0912a2125890f
aeconomica <78719029+aeconomica@users.noreply.github.com>
2023-08-08 11:42:16 +1000

    Add Aqua to test suite (#4)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,14 @@ using Test
 using DataFrames
 import Dates
 
+using Aqua
+
+Aqua.test_all(
+    Aeconomica,
+    ambiguities=false
+)
+Aqua.test_ambiguities(Aeconomica) # The default ambiguity test catches ambiguities in dependencies, which is not what we want to test
+
 # Fetching without setting a key fails
 set_apikey("")
 @test_throws ErrorException("You have not set an `apikey`. Please run `set_apikey(\"YOURKEY\")` first.") fetch_series("CPI")
